0aac27d3d4d02270cfae558706e61763a88bcc3d
bachir <mohamadbachir01@gmail.com>
2021-03-20 01:48:25 +0200

    basic queries

---
+++ answers.sh
@@ -0,0 +1,20 @@
+select name from students;
+
+
+select * from students where age>30;
+
+
+SELECT name from students WHERE Gender="F" AND  Age=30;
+
+
+SELECT Points from students WHERE name="Alex";
+
+
+INSERT INTO students (name,Age,Gender,Points) values ("bachir",21,"M",000);
+
+
+UPDATE students SET Points=505 WHERE name="Basma";
+
+
+UPDATE students SET Points=194 WHERE name="Alex";
+
